31d8fe4e7f9bfd8ad400f2fb048597d0f2265fc0
Jay Kasiphat <kasiphatpir@gmail.com>
2022-04-07 02:34:23 +0700

    Clean code

--- main.js
+++ main.js
@@ -1,46 +1,84 @@
-// Form validation
-const throwError = (message) => {
+// TODO: Don't round lotSize up
+// TODO: Clean & refactor
+// TODO: Add comments
+
+function throwError(message) {
   throw new Error(message);
 }
 
-const convertCurrency = async (from, to, amount) => {
-  const requestURL = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`;
-  let response = await fetch(requestURL);
+function validForm(arr) {
+  // Check if inputs are numbers without 0
+  return (
+    !arr.includes(0)
+    && arr.every((num) => !Number.isNaN(num))
+  );
+}
+
+async function convertCurrency(from, to, amount) {
+  const requestURL = `https://api.exchangerate.host/`
+                      + `convert?from=${from}&to=${to}&amount=${amount}`;
+  const response = await fetch(requestURL);
 
   if (response.ok) {
     return response;
   }
-  
+
   throwError(response.status);
 }
 
-const calculateLot = async () => {
-  let index = document.querySelector('#index-select').value;
-  let risk = Number(document.querySelector('#risk-input').value);
-  let accountSize = Number(document.querySelector('#account-size-input').value);
-  let sl = Number(document.querySelector('#stop-loss-input').value);
-  let contractSize = Number(document.querySelector('#contract-size-select').value);
+async function calculateLot() {
+  const index = document.querySelector("#index-select").value;
+  const risk = Number(
+    document.querySelector("#risk-input").value
+  );
+  const accountSize = Number(
+    document.querySelector("#account-size-input").value
+  );
+  const sl = Number(
+    document.querySelector("#stop-loss-input").value
+  );
+  const contractSize = Number(
+    document.querySelector("#contract-size-select").value
+  );
+  const positionArr = [index, risk, accountSize, sl, contractSize];
 
-  if (index === 'us30' || index === 'nas100') {
+  if (!validForm(positionArr)) {
+    alert("The form is not valid. Please check your data.");
+    throwError("The form is not valid");
+  }
+
+  if (index === "us30" || index === "nas100") {
     const lotSize = ((risk * 0.01 * accountSize) / sl) / contractSize;
     return Math.round((lotSize + Number.EPSILON) * 100) / 100;
-  } else if (index === 'de30') {
-    const riskPerPointInUSD = (risk * 0.01 * accountSize) / sl;    
-    const fetchResult = await convertCurrency('USD', 'EUR', riskPerPointInUSD);    
-    const json = await fetchResult.json();    
-    const riskPerPointInEUR = json.result || throwError('Rate conversion is null');
+  } else if (index === "de30") {
+    const riskPerPointInUSD = (risk * 0.01 * accountSize) / sl;
+    const fetchResult = await convertCurrency("USD", "EUR", riskPerPointInUSD);
+    const json = await fetchResult.json();
+    const riskPerPointInEUR = json.result
+                              || throwError("Rate conversion is null");
     const lotSize = riskPerPointInEUR / contractSize;
+
+    console.log(`Risk per point (USD): ${riskPerPointInUSD}`);
+    console.log(`Risk per point (EUR): ${riskPerPointInEUR}`);
+
     return Math.round((lotSize + Number.EPSILON) * 100) / 100;
   }
 }
 
-const displayLot = async () => {
-  let lotSizeInput = document.querySelector('#lot-size-input');
-  lotSizeInput.value = '';
-  lotSizeInput.setAttribute('placeholder', 'Loading...')
-  let lotSize = await calculateLot();  
-  lotSizeInput.value = lotSize;
+async function displayLot() {
+  const lotSizeInput = document.querySelector("#lot-size-input");
+  lotSizeInput.value = "";
+  lotSizeInput.setAttribute("placeholder", "Loading...");
+  try {
+    const lotSize = await calculateLot();
+    lotSizeInput.value = lotSize;
+  } catch (error) {
+    lotSizeInput.setAttribute(
+      "placeholder", "Invalid Data. Please try again."
+    );
+    console.log(error);
+  }
 }
 
-let calculateButton = document.querySelector('#calculateBtn');
-calculateButton.addEventListener('click', displayLot);
+const calculateButton = document.querySelector("#calculateBtn");
+calculateButton.addEventListener("click", displayLot);
